46571a377a5a3fb7e80fb34b0f7cba7106dd160c
Omar Jatoi <git@omarjatoi.com>
2026-04-05 10:41:54 -0400

    Use flake-utils, and set formatter

--- flake.nix
+++ flake.nix
@@ -1,42 +1,33 @@
 {
   description = "Lightweight, standalone C++ inference engine for Google's Gemma models";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
   outputs =
-    { self, nixpkgs }:
-    let
-      forAllSystems = nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-        "aarch64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-      ];
-    in
     {
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            packages = [
-              pkgs.nixfmt
-            ];
-          };
-        }
-      );
-
-      packages = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = {
           gemma-cpp = pkgs.callPackage ./package.nix { };
           default = self.packages.${system}.gemma-cpp;
-        }
-      );
-    };
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.nixfmt ];
+        };
+
+        formatter = pkgs.nixfmt;
+      }
+    );
 }
